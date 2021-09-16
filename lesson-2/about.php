73c26d257b840761e58e0f9594e12edfe0c1e12b
Petr Komlev <petr.komlev@gmail.com>
2021-09-16 22:26:02 +0300

    lesson-2

---
+++ lesson-2/about.php
@@ -0,0 +1,3 @@
+<?=$content?>
+<h1>About</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt sit doloremque sequi voluptas magnam sapiente, provident esse nulla in, eligendi soluta reiciendis vero unde blanditiis nisi architecto numquam possimus velit?</p>
